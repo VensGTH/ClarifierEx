36dee5c736f029b4d28c55f895f75fd95611e4d1
Johan Hernandez <hdzjohan@gmail.com>
2020-08-10 12:21:47 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-call "%VS140COMNTOOLS%\VsDevCmd.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
 msbuild .\ConfuserEx\Confuser2.sln /t:Build /p:Configuration=Debug
 msbuild .\ConfuserEx\Confuser2.sln /t:Build /p:Configuration=Release
 pause
